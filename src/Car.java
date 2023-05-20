f981ca91e191285d159bc4d49d1e6fc02260ec54
aorandomcrew <danbogomolov@gmail.com>
2023-05-20 14:34:07 +0300

    Final

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends EngineTransport {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,5 @@
+public class ServiceStation {
+    public void check(Service service) {
+        service.doService();
+    }
+}
